2f4fe352197afe75cdb23befcf861cca0ce65df5
macmar22 <maciej.marucha@gmail.com>
2021-02-10 18:50:19 +0100

    First

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-PHP Laravel Online Shop App  
-Initial data is in initial.txt file
+PHP Laravel Online Shop App with Laratrust
+Panel for user and admin
+Initial data is in initial.txt file 
+
+Skip or change database name in .env file
+
+go to the project directory in terminal
+
+composer require santigarcor/laratrust
+php artisan vendor:publish --tag="laratrust"
+php artisan config:clear
+composer dump-autoload
+php artisan vendor:publish --tag="laratrust-seeder"
+composer dump-autoload
+php artisan migrate
+php artisan db:seed
+
+php artisan serve
--- database/migrations/2021_01_21_205020_create_pizzas_table.php
+++ database/migrations/2021_01_21_205020_create_pizzas_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 class CreatePizzasTable extends Migration
@@ -21,6 +22,13 @@ class CreatePizzasTable extends Migration
             $table->float('price_m',6,2);
             $table->float('price_l',6,2);
         });
+
+        DB::table('pizzas')->insert(['id' => 1, 'name' => 'MARGHERITA', 'ingredients' => 'sos, ser', 'price_s' => 9, 'price_m' => 16, 'price_l' => 30]);
+        DB::table('pizzas')->insert(['id' => 2, 'name' => 'SICILIA', 'ingredients' => 'sos, ser, szynka, oregano', 'price_s' => 12, 'price_m' => 19, 'price_l' => 33]);
+        DB::table('pizzas')->insert(['id' => 3, 'name' => 'CAPRICCIOSA', 'ingredients' => 'sos, ser, szynka, pieczarki', 'price_s' => 13, 'price_m' => 20, 'price_l' => 34]);
+        DB::table('pizzas')->insert(['id' => 4, 'name' => 'SALAMI', 'ingredients' => 'sos, ser, salami', 'price_s' => 13, 'price_m' => 20, 'price_l' => 34]);
+        DB::table('pizzas')->insert(['id' => 5, 'name' => 'BACON', 'ingredients' => 'sos, ser, boczek, cebula, ogórek konserwowy', 'price_s' => 14, 'price_m' => 21, 'price_l' => 35]);
+        DB::table('pizzas')->insert(['id' => 6, 'name' => 'POLLO', 'ingredients' => 'sos, ser, kurczak, pieczarka, kukurydza', 'price_s' => 14, 'price_m' => 21, 'price_l' => 35]);
     }
 
     /**
